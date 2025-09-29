665c2623deec26b8a4e3948003985a420f8a477e
Vipperla Rakshita <143200833+rakshita0412@users.noreply.github.com>
2025-09-30 02:31:10 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,16 +1,12 @@
 import os
-import cv2
 import streamlit as st
 from PIL import Image
 from inference import load_csrnet_model, get_count_and_heatmap
 
-
 @st.cache_resource
 def get_model():
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
-    MODEL_PATH = os.path.join(BASE_DIR, "csrnet_model", "csrnet_train.pth")
-    model = load_csrnet_model(MODEL_PATH)
-    return model
+    MODEL_PATH = os.path.join(os.path.dirname(__file__), "../csrnet_model/csrnet_train.pth")
+    return load_csrnet_model(MODEL_PATH)
 
 model = get_model()
 
@@ -18,13 +14,11 @@ st.title("👥 CSRNet Crowd Counting")
 st.write("Upload an image to estimate crowd count and visualize heatmap.")
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert("RGB")
     st.image(img, caption="Uploaded Image", use_column_width=True)
 
     overlay, count = get_count_and_heatmap(model, img)
+    st.image(overlay, caption=f"Estimated Count: {count}", use_column_width=True)
+    st.success(f"Estimated Crowd Count: {count}")
 
-    st.image(overlay, caption=f"Estimated Count: {count:.2f}", use_column_width=True)
-    st.success(f"Estimated Crowd Count: {count:.2f}")
-    
