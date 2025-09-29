3ec4e86eac3d12dbed0adfae382df811aad9ecb4
Vipperla Rakshita <143200833+rakshita0412@users.noreply.github.com>
2025-09-30 01:20:32 +0530

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+from inference import load_csrnet_model, get_count_and_heatmap
+
+@st.cache_resource
+def get_model():
+    return load_csrnet_model("./csrnet_train.pth")
+
+model = get_model()
+
+st.title("👥 CSRNet Crowd Counting")
+st.write("Upload an image to estimate crowd count and visualize heatmap.")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file).convert("RGB")
+    st.image(img, caption="Uploaded Image", use_column_width=True)
+
+    overlay, count = get_count_and_heatmap(model, img)
+
+    st.image(overlay, caption=f"Estimated Count: {count:.2f}", use_column_width=True)
+    st.success(f"Estimated Crowd Count: {count:.2f}")
