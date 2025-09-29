c7a4e62e4e3f125192503cd35133e5c01a55a088
Vipperla Rakshita <143200833+rakshita0412@users.noreply.github.com>
2025-09-30 02:30:08 +0530

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -3,9 +3,7 @@ import numpy as np
 import cv2
 from PIL import Image
 import torchvision.transforms as transforms
-from training import CSRNet
-
-print(cv2.__version__)
+from training import CSRNet  
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -20,17 +18,22 @@ def load_csrnet_model(pth_path="../csrnet_model/csrnet_train.pth"):
     model.eval()
     return model
 
-def get_count_and_heatmap(model, image: Image.Image):
+def get_count_and_heatmap(model, image: Image.Image, scale_small_crowds=True):
     img_tensor = transform(image).unsqueeze(0).to(device)
+
     with torch.no_grad():
         pred = model(img_tensor)
+
     density_map = pred.squeeze().cpu().numpy()
-    
+
     density_map = np.clip(density_map, 0, None)
-    
+
     est_count = float(density_map.sum())
-    est_count_scaled = est_count * 0.55
-    est_count_rounded = round(est_count)  
+
+    if scale_small_crowds and est_count < 20:
+        est_count *= 0.6
+
+    est_count_rounded = round(est_count)
 
     heatmap = density_map / (density_map.max() + 1e-8)
     heatmap = (heatmap * 255).astype(np.uint8)
@@ -40,4 +43,5 @@ def get_count_and_heatmap(model, image: Image.Image):
     img_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     overlay = cv2.addWeighted(img_cv, 0.6, heatmap_color, 0.4, 0)
     overlay_rgb = cv2.cvtColor(overlay, cv2.COLOR_BGR2RGB)
-    return overlay_rgb, est_count_rounded  
+
+    return overlay_rgb, est_count_rounded
